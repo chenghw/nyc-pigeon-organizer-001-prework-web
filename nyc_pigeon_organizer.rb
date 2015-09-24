f2c5198f2104618570f6a6d4195980894e521a5f
chenghw <ho.won.cheng@gmail.com>
2015-09-24 19:09:45 -0400

    pigeon complete

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, detail|
+    detail.each do |specific, names|
+      names.each do |name|
+        if pigeon_list.has_key?(name)
+          if pigeon_list[name].has_key?(category)
+            pigeon_list[name][category] << specific.to_s
+          else
+            pigeon_list[name][category] = [specific.to_s]
+          end
+        else
+          pigeon_list[name] = {}
+          pigeon_list[name][category] = [specific.to_s]  
+        end
+      end
+    end
+  end
+
+  pigeon_list
 end
